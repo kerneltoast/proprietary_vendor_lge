edba85daef0b4fc9048cdeaf86697323fe5b2eea
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-26 14:17:24 -0500

    v4xx: update widevine from hammerhead-mra58k
    Change-Id: I324887a3d7a9aa6ce2604ab6c6ad179ffeaf14aa

--- v4xx-common/v4xx-common-vendor-blobs.mk
+++ v4xx-common/v4xx-common-vendor-blobs.mk
@@ -107,18 +107,17 @@ PRODUCT_COPY_FILES += \
     vendor/lge/v4xx-common/proprietary/vendor/lib/libqomx_jpegenc.so:system/vendor/lib/libqomx_jpegenc.so \
     vendor/lge/v4xx-common/proprietary/bin/qseecomd:system/bin/qseecomd \
     vendor/lge/v4xx-common/proprietary/lib/drm/libfwdlockengine.so:system/lib/drm/libfwdlockengine.so \
+    vendor/lge/v4xx-common/proprietary/lib/libdrmdiag.so:system/lib/libdrmdiag.so \
+    vendor/lge/v4xx-common/proprietary/lib/libdrmfs.so:system/lib/libdrmfs.so \
+    vendor/lge/v4xx-common/proprietary/lib/libdrmtime.so:system/lib/libdrmtime.so \
     vendor/lge/v4xx-common/proprietary/lib/liboemcrypto.so:system/lib/liboemcrypto.so \
+    vendor/lge/v4xx-common/proprietary/lib/libQSEEComAPI.so:system/lib/libQSEEComAPI.so \
+    vendor/lge/v4xx-common/proprietary/lib/librpmb.so:system/lib/librpmb.so \
+    vendor/lge/v4xx-common/proprietary/lib/libssd.so:system/lib/libssd.so \
     vendor/lge/v4xx-common/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-    vendor/lge/v4xx-common/proprietary/vendor/lib/libQSEEComAPI.so:system/vendor/lib/libQSEEComAPI.so \
-    vendor/lge/v4xx-common/proprietary/vendor/lib/libStDrvInt.so:system/vendor/lib/libStDrvInt.so \
     vendor/lge/v4xx-common/proprietary/vendor/lib/libWVphoneAPI.so:system/vendor/lib/libWVphoneAPI.so \
     vendor/lge/v4xx-common/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
     vendor/lge/v4xx-common/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
-    vendor/lge/v4xx-common/proprietary/vendor/lib/libdrmdiag.so:system/vendor/lib/libdrmdiag.so \
-    vendor/lge/v4xx-common/proprietary/vendor/lib/libdrmfs.so:system/vendor/lib/libdrmfs.so \
-    vendor/lge/v4xx-common/proprietary/vendor/lib/libdrmtime.so:system/vendor/lib/libdrmtime.so \
-    vendor/lge/v4xx-common/proprietary/vendor/lib/librpmb.so:system/vendor/lib/librpmb.so \
-    vendor/lge/v4xx-common/proprietary/vendor/lib/libssd.so:system/vendor/lib/libssd.so \
     vendor/lge/v4xx-common/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
     vendor/lge/v4xx-common/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
     vendor/lge/v4xx-common/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
